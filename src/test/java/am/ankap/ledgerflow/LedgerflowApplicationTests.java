a2dc5efa5e1868f89471a8fc57526569ff998060
vaheh <vahe.h.x@gmail.com>
2026-08-04 23:11:34 +0400

    project creation

---
+++ src/test/java/am/ankap/ledgerflow/LedgerflowApplicationTests.java
@@ -0,0 +1,13 @@
+package am.ankap.ledgerflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
